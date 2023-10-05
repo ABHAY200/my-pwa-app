a48fc139d3db71d4ef3c13c4212b2694635c575a
Abhay <abhay@keyvalue.systems>
2023-10-05 15:10:44 +0530

    fix: service worker file revert

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from './pages/dashboard/dashboard.tsx'; // Import the ShopList 
 function App() {
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
