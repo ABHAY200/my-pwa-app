7044a1c7d8d42d63fe40807b6ce508c07cae6477
Abhay <abhay@keyvalue.systems>
2023-10-06 11:52:21 +0530

    fix: service-worker file added in public folder

--- src/App.js
+++ src/App.js
@@ -5,43 +5,8 @@ import './App.css'; // Import your main CSS file
 import Dashboard from './pages/dashboard/dashboard.tsx'; // Import the ShopList component
 
 function App() {
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  }
-
-  let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  console.log('beforeinstallprompt');
-  deferredPrompt = e;
-  // Show install button or prompt here
-});
-
-const handleInstallButtonClick = () => {
-  console.log('deferredPrompt', deferredPrompt);
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User installed the PWA');
-      }
-      deferredPrompt = null;
-    });
-  }
-};
-
-  
   return (
     <div className="App">
-      <button onClick={handleInstallButtonClick}>Install PWA</button>
       <Dashboard /> {/* Render the ShopList component */}
     </div>
   );
